4419724460e8c1d1db78cfe704d9402bb02f5a2e
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-12-10 20:25:25 +0700

    fix: delete comment use case

--- src/Applications/use_case/DeleteThreadCommentUseCase.js
+++ src/Applications/use_case/DeleteThreadCommentUseCase.js
@@ -1,6 +1,3 @@
-const ForbiddenError = require('../../Commons/exceptions/ForbiddenError');
-const NotFoundError = require('../../Commons/exceptions/NotFoundError');
-
 class DeleteThreadCommentUseCase {
   constructor({
     threadRepository,
@@ -13,16 +10,16 @@ class DeleteThreadCommentUseCase {
   async execute(userIdPayload, threadIdPayload, useCasePayload) {
     const thread = await this._threadRepository.findThreadById(threadIdPayload);
     if (!thread) {
-      throw new NotFoundError('FIND_THREAD.ID_THREAD_IS_NOT_FOUND');
+      throw new Error('FIND_THREAD.ID_THREAD_IS_NOT_FOUND');
     }
 
     const comment = await this._threadCommentRepository.findCommentById(useCasePayload);
     if (!comment) {
-      throw new NotFoundError('FIND_COMMENT.ID_COMMENT_IS_NOT_FOUND');
+      throw new Error('FIND_COMMENT.ID_COMMENT_IS_NOT_FOUND');
     }
 
     if (comment.user_id !== userIdPayload) {
-      throw new ForbiddenError('DELETE_COMMENT.COMMENT_IS_NOT_OWNED_BY_USER');
+      throw new Error('DELETE_COMMENT.COMMENT_IS_NOT_OWNED_BY_USER');
     }
 
     return await this._threadCommentRepository
--- src/Applications/use_case/_test/DeleteThreadCommentUseCase.test.js
+++ src/Applications/use_case/_test/DeleteThreadCommentUseCase.test.js
@@ -1,5 +1,3 @@
-const ForbiddenError = require('../../../Commons/exceptions/ForbiddenError');
-const NotFoundError = require('../../../Commons/exceptions/NotFoundError');
 const ThreadCommentRepository = require('../../../Domains/thread-comments/ThreadCommentRepository');
 const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
 const DeleteThreadCommentUseCase = require('../DeleteThreadCommentUseCase');
@@ -22,7 +20,9 @@ describe('DeleteThreadCommentUseCase', () =>  {
         return threadId == 'thread-123'
           ? {
             id: 'thread-123',
-            body: 'test body'
+            body: 'test body',
+            created_at: new Date(),
+            user_id: 'user-123',
           }
           : null;
       });
@@ -31,8 +31,12 @@ describe('DeleteThreadCommentUseCase', () =>  {
         return commentId == 'comment-123'
           ? {
             id: 'comment-123',
-            contet: 'test body',
+            created_at: new Date(),
+            deleted_at: null,
+            is_delete: false,
             user_id: 'user-123',
+            content: 'test body',
+            thread_id: 'thread-123'
           }
           : null;
       });
@@ -51,15 +55,15 @@ describe('DeleteThreadCommentUseCase', () =>  {
     expect(mockThreadCommentRepository.softDeleteCommentById).toBeCalledWith(validUseCasePayload);
 
     await expect(getThreadCommentUseCase.execute(validUserId, invalidThreadId, validUseCasePayload))
-      .rejects.toThrow(NotFoundError);
+      .rejects.toThrow(Error);
     expect(mockThreadRepository.findThreadById).toBeCalledWith(invalidThreadId);
 
     await expect(getThreadCommentUseCase.execute(validUserId, validThreadId, invalidThreadId))
-      .rejects.toThrow(NotFoundError);
+      .rejects.toThrow(Error);
     expect(mockThreadCommentRepository.findCommentById).toBeCalledWith(invalidThreadId);
 
     await expect(getThreadCommentUseCase.execute(invalidUserId, validThreadId, validUseCasePayload))
-      .rejects.toThrow(ForbiddenError);
+      .rejects.toThrow(Error);
     expect(mockThreadCommentRepository.findCommentById).toBeCalledWith(validUseCasePayload);
   });
 });
